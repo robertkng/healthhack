d03a7c1766d0ae18818463dd623b8c8da8c7df91
Robert Ng <robertng85@gmail.com>
2017-04-04 18:26:56 -0400

    Set up add contacts form

--- src/components/App/Add/Add.jsx
+++ src/components/App/Add/Add.jsx
@@ -39,46 +39,94 @@ export default class Add extends Component {
     });
   }
 
+  addToDb(e) {
+    fetch('/itinerary/itinerary', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        title: this.state.itinName,
+        itinerary: this.state.itinDescription,
+      })
+    })
+    .then((res) => {
+      this.getAllItineraries();
+      this.reset();
+    })
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="info">
+        <br/>
+        <div className="first-name">
         First Name
+        <br/>
           <input
             id="first-name"
             type="text"
             maxLength="50"
             value={this.state.firstName}
             onChange={this.addFirstName.bind(this)}
-          /><br/>
+            className="form"
+          />
+        </div>
+        <br/>
+        <div className="last-name">
         Last Name
+        <br/>
           <input
             id="last-name"
             type="text"
             maxLength="50"
             value={this.state.lastName}
             onChange={this.addLastName.bind(this)}
-          /><br/>
+            className="form"
+          />
+        </div>
+        <br/>
+        <div className="email">
         Email
+        <br/>
           <input
             id="email"
             type="text"
             maxLength="50"
             value={this.state.email}
             onChange={this.addEmail.bind(this)}
-          /><br/>
+            className="form"
+          />
+        </div>
+        <br/>
+        <div className="phone-number">
         Phone Number
+        <br/>
           <input
             id="phone-number"
             type="text"
             maxLength="10"
             value={this.state.phoneNumber}
             onChange={this.addPhoneNumber.bind(this)}
-          /><br/>
+            className="form"
+          />
+        </div><br/>
+        <div className="status">
         Status
-          <select valueLink="{this.linkState('val')}">
+        <br/>
+          <select
+            valueLink="{this.linkState('val')}"
+            className="form">
             <option value="active">Active</option>
             <option value="inactive">Inactive</option>
-          </select><br/>
+          </select>
+        </div>
+        <button
+          id="onclick"
+          onClick
+        >SUBMIT CONTACT
+        </button>
       </div>
     );
   }
